7c1e393c40889e3cce4fb309399c033aedf257be
Sapna2025 <sapna2006saini@gmail.com>
2026-09-24 12:18:29 +0530

    button click

---
+++ 1b.js
@@ -0,0 +1,12 @@
+const EventEmitter = require("events");
+class Button extends EventEmitter
+{
+click (){
+    this.emit("click");
+}
+}
+const button= new Button();
+button.on('click',()=>{
+    console.log("Button clicked");
+});
+button.click();
